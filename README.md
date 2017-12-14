52e8e2a38d2b94c33e7a3fed94b27a009410cff0
Roy Cai <roysunghan@gmail.com>
2017-12-14 11:23:03 +0800

    ✨ Feat: init project

---
+++ README.md
@@ -0,0 +1,33 @@
+# Merge Images With Every Combination
+
+## Introduction
+
+It is tool that merge images from different combination.
+
+
+
+## Build Exec File
+
+### Preinstall
+
+- pkg
+
+### Build
+
+    pkg .
+
+## Starting Quickly
+
+    ./Node-Combinate-Images-*
+
+Output files to ./assets/output .
+
+## Setting Example
+    {
+      "folders": [
+        "background",
+        "cats"
+      ]
+    }
+
+It is folders from assets that you want to merge images in here. It will merge images for the sort.
---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+console.log(process.version)
+const fs = require('fs')
+const images = require('images')
+const path = require('path')
+const root = process.pkg ? src => path.join(path.dirname(process.execPath), src) : src => path.join(__dirname, src)
+const rootPath = root('./')
+let setting = fs.readFileSync(root('./setting.json'))
+setting = JSON.parse(setting.toString())
+const { folders } = setting
+
+
+
+const foldersFiles = folders.map((folder) => {
+  try {
+    return fs.readdirSync(root(`assets/${folder}`))
+      .filter((file) => {
+        // filter file name that isn't includes `.` in first word.
+        return !/^\./.test(file)
+      })
+      .map(file => `assets/${folder}/${file}`)
+  } catch(e) {
+    return
+  }
+}).filter((folderFiles) => {
+  // remove is not exist.
+  return folderFiles
+})
+
+const combinations = permuteFromSets(foldersFiles)
+combinations.map(combination => mergeImages(combination))
+
+
+function mergeImages(paths) {
+  console.log(paths)
+  let tmpPaths = paths.slice(0)
+  let canvas = images(tmpPaths.splice(0, 1)[0])
+  tmpPaths.forEach((path) => {
+    canvas.draw(images(path), 0, 0)
+  })
+
+  let resultBuffer = canvas.encode("png", {
+    quality: 100
+  });
+
+  let fileName = paths.map(path => path.split('.')[0].match(/[^\/]+/g).pop()).join('-')
+  fileName += '.png'
+  outputFile(root('./assets/output'), fileName, resultBuffer)
+}
+
+function permuteFromSets(sets) {
+  const result = []
+  const lastSetIndex = sets.length - 1
+  combine([], 0)
+  return result
+
+  function combine(arr, setIndex) {
+    sets[setIndex].forEach(set => {
+      const tmpArray = arr.slice(0)
+      tmpArray.push(set)
+      setIndex === lastSetIndex ? result.push(tmpArray) : combine(tmpArray, setIndex + 1)
+    })
+  }
+}
+
+function outputFile(path, fileName, buffer) {
+  fs.writeFile(`${path}/${fileName}`, buffer, (err, written) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("info: Success, Output file " + fileName);
+    }
+  })
+}
+
